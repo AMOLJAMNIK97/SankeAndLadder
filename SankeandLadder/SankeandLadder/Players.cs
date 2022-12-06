5e6a5a11c03582c0505416c2dada96cf2b97786c
Amoljamnik97 <ajamnik.mech@gmail.com>
2022-12-06 19:12:23 +0530

    [amol]- Roll Dice and Get Number

---
+++ SankeandLadder/SankeandLadder/Players.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SankeandLadder
+{
+    internal class Players
+    {
+        
+            private readonly Random rnd = new Random();
+
+            public void  Roll()
+            {
+                int diceResult = rnd.Next(1, 7);
+            }
+
+        
+    }
+}
